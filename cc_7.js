7f26d0d8432b7146321edc8cd41e5735ef310634
Daniel Feliciano-Munoz <danielfelicianomunoz@usf.edu>
2025-02-16 13:21:19 -0500

    Task 2 - Employee Hourly Wage Calculation

--- cc_7.js
+++ cc_7.js
@@ -10,3 +10,17 @@ function calculateInvoice(subtotal, taxRate, discount) {
 // lines below run the function using the parameters provided
 calculateInvoice(100, 0.08, 5);
 calculateInvoice(500, 0.1, 20);
+
+
+// Task 2: Function Expression
+
+// lines below create a function to calculate the hourly wage of an employee
+function calculateHourlyWage(salary, hoursPerWeek) {
+    let hourlyWage = salary/(hoursPerWeek*52);  // creates the equation in the function
+    console.log(`Hourly Wage: $${hourlyWage.toFixed(2)}`);  // this line logs the output in the console
+};
+
+
+// lines below run the function using the parameters provided
+calculateHourlyWage(52000,40);
+calculateHourlyWage(75000,35);
