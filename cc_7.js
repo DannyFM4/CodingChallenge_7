975953ad891b877056f832cd47b46ea78922a94b
Daniel Feliciano-Munoz <danielfelicianomunoz@usf.edu>
2025-02-16 13:33:32 -0500

    Task 3 - Customer Loyalty Discount

--- cc_7.js
+++ cc_7.js
@@ -24,3 +24,21 @@ function calculateHourlyWage(salary, hoursPerWeek) {
 // lines below run the function using the parameters provided
 calculateHourlyWage(52000,40);
 calculateHourlyWage(75000,35);
+
+
+// Task 3: Arrow Function
+
+// lines below create a function to calculate a discount based on the loyalty of the customer
+const calculateLoyaltyDiscount = (amount, years) => {
+    let loyaltyDiscount = 0;
+    if (years >= 5) loyaltyDiscount = amount - (amount * .15); // lines 34-36 use if-else statements to figure out which discount percent to use based on the years
+    else if (years >= 3) loyaltyDiscount = amount - (amount * .1);
+    else loyaltyDiscount = amount - (amount * .05);
+
+    console.log(`Discounted Price: $${loyaltyDiscount.toFixed(2)}`); // this line logs the output in the console
+};
+
+
+// lines below run the function using the parameters provided
+calculateLoyaltyDiscount(100, 6);
+calculateLoyaltyDiscount(200, 2);
