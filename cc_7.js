c9d88bd5cabf804c0afaff3d3270cb880537dd6a
Daniel Feliciano-Munoz <danielfelicianomunoz@usf.edu>
2025-02-16 14:12:44 -0500

    Task 4 - Product Shipping Cost Calculation

--- cc_7.js
+++ cc_7.js
@@ -42,3 +42,24 @@ const calculateLoyaltyDiscount = (amount, years) => {
 // lines below run the function using the parameters provided
 calculateLoyaltyDiscount(100, 6);
 calculateLoyaltyDiscount(200, 2);
+
+
+// Task 4: Parameters and Arguments
+
+// lines below create a function to calculate the shipping cost of an item
+function calculateShippingCost(weight, location, expedited = false) {
+    let rates = { "USA": 5 + (0.5*weight), "Canada": 10 + (0.7*weight)};
+    let shippingCost = (rates[location]); // this line picks the equation out of the object depending on the location
+
+    if (expedited === true) {  // declares that if the recipient chooses to expedite it, then add $10 to the cost
+        shippingCost += 10;
+    };
+    
+    // line 59 tells the function to log the results in the console
+    console.log(`Total Cost: $${shippingCost.toFixed(2)}`);
+}
+
+
+// lines below run the function using the parameters provided
+calculateShippingCost(10, "USA", true);
+calculateShippingCost(5, "Canada", false);
